aa1740464b4d96e8f733d501277ffffac7c1d167
Alex Fallah <fallaha@wit.edu>
2015-06-23 16:50:03 -0700

    Initial Commit

---
+++ maxCalculator/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  maxCalculator
+//
+//  Created by alex fallah on 6/23/15.
+//  Copyright (c) 2015 alex fallah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ maxCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  maxCalculator
+//
+//  Created by alex fallah on 6/23/15.
+//  Copyright (c) 2015 alex fallah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
